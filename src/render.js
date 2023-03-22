a99b748c26d5a12a76ca5be8a67fccb22c5b7efd
Dmitry Shamko <proffery@gmail.com>
2023-03-22 12:53:29 +0300

    style

--- src/render.js
+++ src/render.js
@@ -6,8 +6,9 @@ const render = (() => {
     function projects() {
         for (let i = 0; i < toDoList.getList().length; i++) {
             const projectContainer = document.createElement('div');
-            projectContainer.classList.add(`project${i}`);
+            projectContainer.classList.add(`project`);
             projectContainer.textContent = (`${toDoList.getList()[i].name}`);
+            projectContainer.setAttribute('value', `${i}`)
             progectsContainer.appendChild(projectContainer);
         }
     }
